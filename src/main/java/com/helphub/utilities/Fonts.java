3921398f83ab799f0dda124f70179156fbe142e2
p1etrodev <francodavidpietrokovsky@gmail.com>
2024-09-24 01:41:14 -0300

    fixed Font class

--- src/main/java/com/helphub/utilities/Fonts.java
+++ src/main/java/com/helphub/utilities/Fonts.java
@@ -13,16 +13,21 @@ public class Fonts {
   // Constant font instance for big font size.
   public static final Font BIG;
   // Derived font instances with different sizes based on the BIG font.
-  public static final Font XL = BIG.deriveFont((float) Config.scaleByY(80));   // Extra Large font
-  public static final Font XXL = BIG.deriveFont((float) Config.scaleByY(100));  // Double Extra Large font
-  public static final Font MD = BIG.deriveFont((float) Config.scaleByY(50));    // Medium font
-  public static final Font SM = BIG.deriveFont((float) Config.scaleByY(35));    // Small font
-  public static final Font XS = BIG.deriveFont((float) Config.scaleByY(20));    // Extra Small font
+  public static Font XL;   // Extra Large font
+  public static Font XXL;  // Double Extra Large font
+  public static Font MD;    // Medium font
+  public static Font SM;    // Small font
+  public static Font XS;    // Extra Small font
 
   static {
     try {
       // Loads a custom font "LEMONMILK-Bold.otf" with a scaled size.
       BIG = FontLoader.loadCustomFont("LEMONMILK-Bold.otf", Config.scaleByY(60));
+      XL = BIG.deriveFont((float) Config.scaleByY(80));
+      XXL = BIG.deriveFont((float) Config.scaleByY(100));
+      MD = BIG.deriveFont((float) Config.scaleByY(50));
+      SM = BIG.deriveFont((float) Config.scaleByY(35));
+      XS = BIG.deriveFont((float) Config.scaleByY(20));
     } catch (FontFormatException | IOException e) {
       // Throws a runtime exception if font loading fails.
       throw new RuntimeException(e);
